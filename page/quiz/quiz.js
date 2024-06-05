27b72c15bc8daa450ae1238dd3fea3ae52e3c958
flavianato89 aaaaaaa <escolat716@gmail.com>
2024-06-05 04:06:20 +0000

    alteracoes

--- page/quiz/quiz.js
+++ page/quiz/quiz.js
@@ -8,3 +8,19 @@ botaoTema.addEventListener("click", () => {
 })
 
 verificarTema(body, botaoTema)
+
+const assunto = localStorage.getItem("assunto")
+
+
+function alterarAssunto() {
+     const divIcone = document.querySelector(".assunto_icone")
+     const iconImg = document.querySelector(".assunto_icone img")
+     const assuntoTitulo = document.querySelector(".assunto h1")
+
+     divIcone.classList.add(assunto.toLowerCase())
+     iconImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`);
+     iconImg.setAttribute("alt",  `icone de ${assunto}`)
+     assuntoTitulo.innerText = assunto
+}
+
+alterarAssunto()
